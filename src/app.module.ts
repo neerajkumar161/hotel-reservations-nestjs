5914f8640913b41321ccc55c6508a70f835b1248
Neeraj Kumar <neerajkumar@Neerajs-MBP.lan>
2024-06-17 22:21:19 +0530

    chore: graphql api workflow

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
-import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 import { ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { GraphQLError } from 'graphql';
 import { join } from 'path';
 import { AppController } from './app.controller';
@@ -12,14 +12,18 @@ import { AuthModule } from './auth/auth.module';
 import { HotelModule } from './hotel/hotel.module';
 import { ReservationModule } from './reservation/reservation.module';
 import { UserModule } from './user/user.module';
-
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+      renderPath: '/api-reference',
+      exclude: ['/graphql']
+    }),
     GraphQLModule.forRoot({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-      playground: false,
-      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      playground: true,
+      // plugins: [ApolloServerPluginLandingPageLocalDefault()],
       formatError: (error: GraphQLError) => {
         const graphQLFormattedError = {
           message: error.message,
--- src/common/filters/gql-exception-filter.ts
+++ src/common/filters/gql-exception-filter.ts
@@ -13,9 +13,13 @@ export class GqlExceptionFilter implements ExceptionFilter {
   catch(exception: unknown, host: ArgumentsHost) {
     const gqlHost = GqlArgumentsHost.create(host);
     const ctx = gqlHost.getContext();
-    
 
     if (exception instanceof HttpException) {
+      // Handle Invalid path because of the way Error is thrown in NestJS for GraphQL, Kind of workaround
+      if(exception.message.includes('Cannot GET /')) {
+        return host.switchToHttp().getResponse().send({ message: 'Path not found', code: 404 });
+      }
+
       const status = exception.getStatus();
       const message = exception.message || null;
       throw new ApolloError(message, status.toString());
